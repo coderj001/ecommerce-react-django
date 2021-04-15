09fe5679e6b38ed5df8190732eba0692a77a8ce1
coderj001 <amirajubolchi001@gmail.com>
2021-04-15 22:10:32 +0530

    add logout page to frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingCart,
+  faSignInAlt,
+  faSignOutAlt,
+  faUserAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userActions";
 
 function Navigaton() {
+  const { userInfo } = useSelector((state) => state.userLogin);
+  const dispatch = useDispatch();
+
+  const logoutHandler = (e) => {
+    dispatch(logout());
+  };
+
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -21,12 +35,27 @@ function Navigaton() {
                   Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FontAwesomeIcon icon={faSignInAlt} />
-                  Login
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.username} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>
+                      <FontAwesomeIcon icon={faUserAlt} />
+                      Profile
+                    </NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FontAwesomeIcon icon={faSignInAlt} />
+                    Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
